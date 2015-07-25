eec74ba1984a869b874fe001887c61c96cc094dc
Lauren Smith <LMSmith.715@gmail.com>
2015-07-25 14:57:36 -0400

    refactored

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   "use strict"
 
+  //---Start MODEL-------------
+  //Toggle the view of the menu open and closed
   $('#menu-btn').on('click', function(){
     $('#place-menu').animate({
           width: "toggle"
@@ -8,42 +10,97 @@ $(document).ready(function(){
   $('ul li').toggle();
   });
 
+  //initialize the Google map
   function initialize() {
     var mapOptions = {
       center: { lat: 41.825283, lng: -71.4126816},
       zoom: 16
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    var marker = new google.maps.Marker({
-      position: { lat: 41.8278122, lng: -71.4002354},
-      map: map,
-      title: 'La Creperie'
-      });
-    };
-
-  var viewModel = function(){
-    var self = this;
-
-    var Place = function(data){
-      this.position = ko.observable(data.position);
-      this.title = ko.observable(data.title);
-      this.url = ko.observable(data.url);
-    };
-
-    this.placeList = ko.observableArray([]);
-
-      places.forEach(function(placeItem){
-      self.placeList.push(new Place(placeItem));
-    });
-
-    this.currentPlace = ko.observable(this.placeList()[0]);
-
-    this.setPlace = function(clickedPlace) {
-      self.currentPlace(clickedPlace);
-    };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   };
 
-  ko.applyBindings(new viewModel());
+  var places = [
+    {
+      position: { lat: 41.8278122, lng: -71.4002354},
+      title: 'La Creperie',
+      url: 'http://creperieprov.com/'
+    },
+
+    {
+      position: { lat: 41.825475, lng: -71.413958},
+      title: 'Union Station Brewery',
+      url: 'https://www.johnharvards.com/locations/providence-ri/'
+    },
+
+    {
+      position: { lat: 41.8211319, lng: -71.4119614},
+      title: 'Providence Performing Arts Center',
+      url: 'http://www.ppacri.org/'
+    },
+
+    {
+      position: { lat: 41.826888, lng: -71.407726},
+      title: 'RISD Museum',
+      url: 'http://risdmuseum.org/'
+    },
+
+    {
+      position: { lat: 41.8551243, lng: -71.3616245},
+      title: 'Hope Street Farmers Market',
+      url: 'http://hopestreetmarket.com/'
+    },
+
+    {
+      position: { lat: 41.8186278, lng: -71.4262943},
+      title: 'The Avery',
+      url: 'http://averyprovidence.com/'
+    },
+
+    {
+      position: { lat: 41.8175712, lng: -71.4225758},
+      title: 'Classic Cafe',
+      url: 'http://classiccaferi.com/'
+    },
+
+    {
+      position: { lat: 41.8231192, lng: -71.4054752},
+      title: 'Cable Car Cinema',
+      url: 'http://www.cablecarcinema.com/'
+    },
+
+    {
+      position: { lat: 41.8274751, lng: -71.4138953},
+      title: 'WaterFire',
+      url: 'http://waterfire.org/'
+    },
+
+    {
+      position: { lat: 41.7862761, lng: -71.3933733},
+      title: 'Culinary Arts Museum',
+      url: 'http://www.jwu.edu/content.aspx?id=40194'
+    },
+
+  ];
+
+  //Define the Place class with ko observables
+  var Place = function(data){
+    this.position = ko.observable(data.position);
+    this.title = ko.observable(data.title);
+    this.url = ko.observable(data.url);
+  };
+
+  //---Start VIEWMODEL-------------
+  var ViewModel = function(){
+    var self = this;
+
+    //Store place items in an observable array
+    this.placeList = ko.observableArray([]);
+
+    places.forEach(function(placeItem){
+    self.placeList().push(new Place(placeItem));
+  });
+  };
   google.maps.event.addDomListener(window, 'load', initialize);
+  ko.applyBindings(new ViewModel());
 });
