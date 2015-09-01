3792e1eb088169f21d4c793b966b37df9eeab78b
Lauren Smith <LMSmith.715@gmail.com>
2015-08-31 20:49:30 -0400

    fixed tablet weather

--- js/app.js
+++ js/app.js
@@ -232,6 +232,7 @@ initMap();
     apiCallWunderground.done(function(data) {
         // success
         $('#conditions').append('<p>' +data.current_observation.weather+ '</p>');
+        $('#mobile-conditions').append('<p>' +data.current_observation.weather+ '</p>');
         $('#temp').append('<p>' +data.current_observation.temp_f+ "° F" + '</p>');
         $('#mobile-temp').append('<p>' +data.current_observation.temp_f+ "° F" + '</p>');
     });
